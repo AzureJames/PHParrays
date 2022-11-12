0bf73b6b7ef78c35fc29576c0fc22325bd31ee58
AzureJames <azurethegreat@gmail.com>
2022-11-11 18:20:43 -0700

    validation

---
+++ validate.php
@@ -0,0 +1,12 @@
+<?php 
+$formGood = true;
+if(isset($student) && isset($grade)){
+    if( is_numeric($student) === true || is_numeric($grade) === false ){
+        echo "Grade must be number and student must be a word";
+        $formGood = false;
+    }
+    if($grade > 100 || $grade < 0){ echo "Grade must be between 0 and 100"; $formGood = false; }
+    if(strlen($student) < 3 || strlen($student) > 40){ echo "Student name incorrect length";  $formGood = false;}
+    echo strlen($student);
+}
+?>
